9e3a1a2c1da3305d64db892aa0a92db9a9500a67
Ritik Kumar <madaanritik120@gmail.com>
2024-07-04 12:10:42 -0300

    finished lab 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,146 +1,159 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'BROWSE CATEGORIES',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ),
+      title: 'Flutter List View',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter List View'),
+    );
+  }
+}
 
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Not sure exactly which recipe you are looking for? Do a search or dive into our most popular categories',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )
-              ],
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late TextEditingController _todoController;
+  final List<String> _todoList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _todoController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _todoController.dispose();
+    super.dispose();
+  }
+
+  void _addTodoItem() {
+    setState(() {
+      if (_todoController.text.isNotEmpty) {
+        _todoList.add(_todoController.text);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: const Text("Please enter a valid todo item"),
+            action: SnackBarAction(label: "Ok", onPressed: () {})));
+      }
+      _todoController.clear();
+    });
+  }
+
+  void _removeTodoItem(int index) {
+    setState(() {
+      _todoList.removeAt(index);
+    });
+  }
+
+  void _showRemoveDialog(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Remove Todo'),
+          content: const Text('Do you want to remove this item?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('No'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
             ),
-            // Row 1: Center-aligned text
+            TextButton(
+              child: const Text('Yes'),
+              onPressed: () {
+                _removeTodoItem(index);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
-
-            // Row 2: Left-aligned text
-
-
-            // Row 3: Images with text in the middle
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    'By Meat',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: ElevatedButton(
+                      onPressed: _addTodoItem,
+                      child: const Text("Add"),
+                    ),
                   ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      buildImageStack('Beef', 'images/beef.jpg', circleShape: true),
-                      buildImageStack('Chicken', 'images/chicken.jpg', circleShape: true),
-                      buildImageStack('Pork', 'images/pork.jpg', circleShape: true),
-                      buildImageStack('Seafood', 'images/seafood.jpg', circleShape: true),
-                    ],
+                  Expanded(
+                    child: TextField(
+                      controller: _todoController,
+                      decoration: const InputDecoration(
+                        hintText: "Enter a todo item",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-
-            // Row 4: Images with text at the bottom center
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    'By Course',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      buildImageStack('Main dishes', 'images/maindishes.jpg', textAlignment: Alignment.bottomCenter, circleShape: true),
-                      buildImageStack('Salad Recipes', 'images/saladrecipes.jpg', textAlignment: Alignment.bottomCenter, circleShape: true),
-                      buildImageStack('Side Dishes', 'images/sidedishes.jpg', textAlignment: Alignment.bottomCenter, circleShape: true),
-                      buildImageStack('Crockpot', 'images/crock.jpg', textAlignment: Alignment.bottomCenter, circleShape: true),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-
-            // Row 5: Images with text at the bottom center
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    'By Dessert',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      buildImageStack('Ice cream', 'images/ice.jpg', textAlignment: Alignment.bottomCenter, circleShape: true),
-                      buildImageStack('Brownie', 'images/brownie.jpg', textAlignment: Alignment.bottomCenter, circleShape: true),
-                      buildImageStack('Pies', 'images/pies.jpg', textAlignment: Alignment.bottomCenter, circleShape: true),
-                      buildImageStack('Cookies', 'images/cookies.jpg', textAlignment: Alignment.bottomCenter, circleShape: true),
-                    ],
-                  ),
-                ],
+            Expanded(
+              child: _todoList.isEmpty
+                  ? const Center(
+                child: Text("There are no items in the list"),
+              )
+                  : ListView.builder(
+                itemCount: _todoList.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onLongPress: () => _showRemoveDialog(index),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text('Row number: $index'),
+                          Text(_todoList[index]),
+                        ],
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
           ],
         ),
       ),
-    );
-  }
-
-  // Function to build image stack with text in the middle or at the bottom center
-  Widget buildImageStack(String text, String imagePath, {Alignment textAlignment = Alignment.center, bool circleShape = false}) {
-    return Stack(
-      alignment: textAlignment,
-      children: [
-        Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            shape: circleShape ? BoxShape.circle : BoxShape.rectangle,
-            image: DecorationImage(
-              image: AssetImage(imagePath),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        Container(
-          color: Colors.black54,
-          padding: EdgeInsets.all(8),
-          child: Text(
-            text,
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-        ),
-      ],
     );
   }
 }
